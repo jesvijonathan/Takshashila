ad7336efdf1c783bd6f6baf0dde3482fe1cca645
solomonsanthosh <solosanty2064@gmail.com>
2023-01-26 18:32:21 +0530

    SS

--- app.py
+++ app.py
@@ -6,13 +6,6 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return '<Task %r>' % self.id
 
 
 @app.route('/', methods=['POST'])
