32460a9b46b7460098a091da4c134953de476008
Akash2k20 <akashlearning2020@gmail.com>
2023-01-27 14:08:37 +0530

    dbmodel

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
-from flask import Flask, render_template, url_for, request, redirect
+# pylint: disable=unused-import
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from flask import Flask, render_template, url_for, request, redirect
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql:///uhf2k0slsnu7bmyb:eOn7jbeFcvkIacBucq4J@bodkrvfvmjntqymqaawg-mysql.services.clever-cloud.com:3306/bodkrvfvmjntqymqaawg'
+
 db = SQLAlchemy(app)
 
 
-
+class Registration(db.Model):
+    registration_id = db.Column(db.Integer, nullable= False, primary_key= True)
+    user_id = db.Column(db.Integer, nullable= False)
+    event = db.Column(db.String(100), nullable= False)
+    batch = db.Column(db.String(100), nullable= False)
+    
 @app.route('/', methods=['POST'])
 def index():
-    
+    pass
 
 
 
