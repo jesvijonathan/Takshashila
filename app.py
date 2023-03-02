33d49b50987a73492642c41c7cd5610783232c8e
Jesvi Jonathan <jesvi22j@gmail.com>
2023-03-02 22:28:03 +0530

    ditched the shitty oauth system, new oauth

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from flask import Flask, render_template, jsonify, redirect, url_for, session
+from flask import Flask, render_template, jsonify, redirect, url_for, session, make_response
 from flask_smorest import Api
 from flask_caching import Cache
 from flask_mail import Mail
@@ -28,7 +28,7 @@ load_dotenv(os.path.join(project_folder, '.env'))
 
 
 app = Flask(__name__, static_url_path='/')
-app.app_context().push() 
+app.app_context().push()
 
 app.secret_key = os.getenv('SECRET')
 app.config["API_TITLE"] = "TK2023 Rest API"
@@ -39,6 +39,11 @@ app.config['SQLALCHEMY_POOL_RECYCLE'] = 280
 app.config['SQLALCHEMY_POOL_TIMEOUT'] = 20
 app.config['SQLALCHEMY_POOL_PRE_PING'] = True
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+@app.teardown_request
+def session_clear(exception=None):
+    if exception and db.is_active:
+        db.rollback()
 
 try:
     os.mkdir("/home/takshashila/Takshashila-2023-Backend/static/users")
@@ -76,38 +81,74 @@ def events():
 # @cache.cached(timeout=2)
 def nig():
     return render_template("events.html")
- 
+
 
 blueprint = make_google_blueprint(
     client_id="562058780483-q59qv7347cgqujgebrsf15n6b0u8uhmq.apps.googleusercontent.com",
     client_secret="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",
-    redirect_url= [
-      "http://127.0.0.1:5000",
-      "http://127.0.0.1:5000/auth/callback",
-      "http://127.0.0.1:5000/auth/oauth_redirect",
-      "https://takshashila.pythonanywhere.com",
-      "https://takshashila.pythonanywhere.com/auth/callback",
-      "https://takshashila.pythonanywhere.com/auth/oauth_redirect"
-    ],
-    scope=["profile", "email"],
+    redirect_url="https://takshashila.pythonanywhere.com/google_login",
+    scope = 'openid https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile'
 )
 
 app.register_blueprint(blueprint, url_prefix="/login")
 
+from controllers.userController import createUser_oauth
+
+auth_pop = 0
+domain = "http://takshashila.pythonanywhere.com/"
+# server_url =os.getenv('SERVER_URL')
+server_url = "http://takshashila.pythonanywhere.com/"
+
 @app.route("/google_login")
-def index():
+def google_():
     if not google.authorized:
         return redirect(url_for("google.login"))
     resp = google.get("/oauth2/v2/userinfo")
     assert resp.ok, resp.text
     email = resp.json()["email"]
+
+    res_data= resp.json()
+
+    data = {
+        'email':res_data.get('email', None),
+        'first_name':res_data.get('given_name', None),
+        'last_name': res_data.get('family_name', None),
+        'google_id':res_data.get('id', None)
+    }
+
+    user_db_data = createUser_oauth(data)
+
+    resp = None
+    redirect_url=domain
+    f =0
+    if not user_db_data.stage_two:
+        redirect_url=domain+"user_details"
+        f =1
+
+    if auth_pop == 1:
+        resp = make_response(render_template("oauth_redirect_home.html", redirect=redirect_url, mini=auth_pop))
+
+    else:
+        resp = make_response(redirect(redirect_url))
+        #resp = make_response(render_template("oauth_redirect_home.html", redirect=redirect_url))
+
+    resp.set_cookie('oauth_redirect', redirect_url, secure=True, samesite='Lax')
+    resp.set_cookie('logged_In', "true", secure=True, samesite='Lax')
+    if f ==1:resp.set_cookie('user_details', '', secure=True, samesite='Lax')
+    resp.set_cookie('first_name', res_data.get('given_name'), secure=True, samesite='Lax')
+    resp.set_cookie('last_name', res_data.get('family_name', ""), secure=True, samesite='Lax')
+    resp.set_cookie('email', res_data.get('email'), secure=True, samesite='Lax')
+    resp.set_cookie('hash', user_db_data.hash, secure=True, samesite='Lax')
+
+    return resp
+
     return f"Logged in as {email}"
 
 
 
 @app.errorhandler(404)
 # @cache.cached(timeout=2)
-def page_not_found(e): 
+def page_not_found(e):
     return jsonify(error=str(e)), 404
     return render_template('404.html', error=e, console_print=e), 404
 
@@ -121,4 +162,4 @@ api.register_blueprint(feedBackBlp)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True, threaded=False)
-    
+
--- appp.py
+++ appp.py
@@ -8,15 +8,8 @@ app.secret_key = "mysecretkey"
 blueprint = make_google_blueprint(
     client_id="562058780483-q59qv7347cgqujgebrsf15n6b0u8uhmq.apps.googleusercontent.com",
     client_secret="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",
-    redirect_url= [
-      "http://127.0.0.1:5000",
-      "http://127.0.0.1:5000/auth/callback",
-      "http://127.0.0.1:5000/auth/oauth_redirect",
-      "https://takshashila.pythonanywhere.com",
-      "https://takshashila.pythonanywhere.com/auth/callback",
-      "https://takshashila.pythonanywhere.com/auth/oauth_redirect"
-    ],
-    scope=["profile", "email"],
+    redirect_url= "http://127.0.0.1:5000",
+    scope = 'openid https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile'  
 )
 
 app.register_blueprint(blueprint, url_prefix="/login")
@@ -27,10 +20,21 @@ def index():
         return redirect(url_for("google.login"))
     resp = google.get("/oauth2/v2/userinfo")
     assert resp.ok, resp.text
+    print(resp.json())
     email = resp.json()["email"]
-    return f"Logged in as {email}"
+    return f"Logged in as {resp.json()}"
+
+
+@app.route("/nig")
+def cock():
+    if not google.authorized:
+        return "NOT LOGGED"
+    resp = google.get("/oauth2/v2/userinfo")
+    assert resp.ok, resp.text
+    print(resp.json())
+    email = resp.json()["email"]
+    return f"Logged in as {resp.json()}"
 
 if __name__ == "__main__":
-    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-    os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1' 
     app.run()
