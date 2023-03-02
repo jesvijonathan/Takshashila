804cfcdeba549aafd7aa49fb632e32bf49380e44
Jesvi Jonathan <jesvi22j@gmail.com>
2023-03-02 15:30:09 +0530

    just a fucking / missing

--- blueprints/loginBlp.py
+++ blueprints/loginBlp.py
@@ -18,9 +18,9 @@ loginBlp = Blueprint("loginBlp", __name__, url_prefix='/auth')
 
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 auth_pop = 0
-domain = "http://takshashila.pythonanywhere.com"
+domain = "http://takshashila.pythonanywhere.com/"
 # server_url =os.getenv('SERVER_URL')
-server_url = "http://takshashila.pythonanywhere.com"
+server_url = "http://takshashila.pythonanywhere.com/"
 
 GOOGLE_CLIENT_ID = "562058780483-q59qv7347cgqujgebrsf15n6b0u8uhmq.apps.googleusercontent.com"
 client_secrets_file = os.path.join(
@@ -30,7 +30,7 @@ flow = Flow.from_client_secrets_file(
     client_secrets_file=client_secrets_file,
     scopes=["https://www.googleapis.com/auth/userinfo.profile",
             "https://www.googleapis.com/auth/userinfo.email", "openid"],
-    redirect_uri= str(server_url + "/auth/callback")
+    redirect_uri= str(server_url + "auth/callback")
 )
 
 def login_auth_url():
