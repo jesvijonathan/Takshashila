21318edf89acb906c4c778f2a8523356af35f0ac
Jesvi Jonathan <jesvi22j@gmail.com>
2023-03-08 13:03:02 +0530

    fix crash, added event download

--- text_edit.py
+++ text_edit.py
@@ -5,14 +5,14 @@ app = Flask(__name__, static_url_path='/')
 
 @app.route('/')
 def index():
-    with open('./static/pevents.json', 'r') as f:
+    with open('./static/events.json', 'r') as f:
         data = json.load(f)
     return render_template('edit.html', data=json.dumps(data, indent=4))
 
 @app.route('/update', methods=['POST'])
 def update():
     new_data = request.json
-    with open('./static/pevents.json', 'w') as f:
+    with open('./static/events.json', 'w') as f:
         json.dump(new_data, f)
     return jsonify(success=True)
 
