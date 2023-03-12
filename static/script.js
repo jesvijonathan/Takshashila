2a753d42abee3b43710c2ae641a2e36605906b14
Jesvi Jonathan <jesvi22j@gmail.com>
2023-03-12 22:55:28 +0530

    .

--- static/script.js
+++ static/script.js
@@ -441,11 +441,12 @@ function graphic() {
   // }
 }
 
+// PWA Conifg //
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
     .register("service-worker.js")
     .then((registration) => {
-      console.log("SW Registered!");
+      // console.log("SW Registered!");
     })
     .catch((error) => {
       console.log("SW Registration Failed");
