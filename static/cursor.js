8b0f678bcd3620a9a1e4b6ed324583f33ae4e0a1
Jesvi Jonathan <jesvi22j@gmail.com>
2023-03-16 06:03:06 +0530

    minified cursor

--- static/cursor.js
+++ static/cursor.js
@@ -5,71 +5,50 @@ try {
 }
 const image1 = document.querySelector("#image1"),
   image2 = document.querySelector("#image2"),
-  image5 = document.querySelector("#image5");
-
-const cursor = document.querySelector("#cursor"),
+  image5 = document.querySelector("#image5"),
+  cursor = document.querySelector("#cursor"),
   cursor_border = document.querySelector("#cursor_border"),
   cursor_circle = document.querySelector("#cursor_circle");
-
-document.addEventListener("mousemove", (event) => {
-  const mouseX = event.clientX,
-    mouseY = event.clientY;
-  (cursor.style.transform = `translate3d(${mouseX - 20}px, ${
-    mouseY - 20
-  }px, 0)`),
-    (cursor.style.transform = `translate3d(${mouseX - 20}px, ${
-      mouseY - 20
-    }px, 0)`);
-  let y = mouseY / window.innerHeight,
-    x = mouseX / window.innerHeight;
-
-  (image1.style.transform = `translate(${-2 * x}vw, ${1 * y}vw)`),
-    (image1.style.transform = `scale(1) translate(${-80 * x}px, ${20 * y}px)`),
-    (image2.style.transform = `scale(1.5) rotate(${-14 * x}deg) translate(${
-      70 * x
-    }px, ${180 * y}px)`),
-    (image5.style.transform = `translate(${-7 * x}px, ${-7 * y}px)`);
-
-  $(".link_cur").hover(
-    function () {
-      y <= 0.5
-        ? (cursor_circle.style.transform = "scale(7) translate(10px, 10px)")
-        : (cursor_circle.style.transform = "scale(7) translate(10px, -10px)"),
-        (cursor_circle.innerHTML = $(this).data("text"));
-    },
-    function () {
-      (cursor_circle.style.transform = "scale(1)"),
-        (cursor_circle.innerHTML = "");
-    }
-  );
-
-  document.addEventListener("mousedown", function () {
-    cursor_circle.classList.add("cursor_circle_click");
-    cursor_border.style.background = "white";
-    // document.body.style.background = "white";
-
-    // document.body.style.mixBlendMode = " difference";
-
-    // setTimeout(function () {
-    //   cursor_circle.style.transform = `scale(1)`;
-    // }, 100);
-  });
-
-  document.addEventListener("mouseup", function () {
-    cursor_circle.classList.remove("cursor_circle_click");
-    cursor_border.style.background = "transparent";
-
-    // document.body.style.mixBlendMode = " unset";
-    // document.body.style.background = "#282828 ";
-  });
-});
-
-document.addEventListener("mouseout", () => {
-  let e = event.clientX,
-    n = event.clientY;
-  (n <= 0 || e <= 0 || e >= window.innerWidth || n >= window.innerHeight) &&
-    (cursor.style.display = "none");
+document.addEventListener("mousemove", (r) => {
+  let e = r.clientX,
+    t = r.clientY;
+  (cursor.style.transform = `translate3d(${e - 20}px, ${t - 20}px, 0)`),
+    (cursor.style.transform = `translate3d(${e - 20}px, ${t - 20}px, 0)`);
+  let c = t / window.innerHeight,
+    s = e / window.innerHeight;
+  (image1.style.transform = `translate(${-2 * s}vw, ${1 * c}vw)`),
+    (image1.style.transform = `scale(1) translate(${-80 * s}px, ${20 * c}px)`),
+    (image2.style.transform = `scale(1.5) rotate(${-14 * s}deg) translate(${
+      70 * s
+    }px, ${180 * c}px)`),
+    (image5.style.transform = `translate(${-7 * s}px, ${-7 * c}px)`),
+    $(".link_cur").hover(
+      function () {
+        c <= 0.5
+          ? (cursor_circle.style.transform = "scale(7) translate(10px, 10px)")
+          : (cursor_circle.style.transform = "scale(7) translate(10px, -10px)"),
+          (cursor_circle.innerHTML = $(this).data("text"));
+      },
+      function () {
+        (cursor_circle.style.transform = "scale(1)"),
+          (cursor_circle.innerHTML = "");
+      }
+    ),
+    document.addEventListener("mousedown", function () {
+      cursor_circle.classList.add("cursor_circle_click"),
+        (cursor_border.style.background = "white");
+    }),
+    document.addEventListener("mouseup", function () {
+      cursor_circle.classList.remove("cursor_circle_click"),
+        (cursor_border.style.background = "transparent");
+    });
 }),
+  document.addEventListener("mouseout", () => {
+    let r = event.clientX,
+      e = event.clientY;
+    (e <= 0 || r <= 0 || r >= window.innerWidth || e >= window.innerHeight) &&
+      (cursor.style.display = "none");
+  }),
   document.addEventListener("mouseenter", () => {
     cursor.style.display = "block";
   });
